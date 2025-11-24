025922f49d303db4448dd367933c4af3d07fc315
KEERTHIVASAN-1 <k32304983@gmail.com>
2025-11-25 02:10:35 +0530

    Added premium loading animation

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// REMOVE PREMIUM LOADER AFTER REACT LOADS
+window.addEventListener("load", () => {
+  const loader = document.getElementById("loader");
+  if (loader) {
+    setTimeout(() => loader.classList.add("hidden"), 500);
+  }
+});
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
